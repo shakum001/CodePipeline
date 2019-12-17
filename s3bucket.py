83fe91b0183a81abf8f7a26ae58016725ff6009a
Shailesh Kumar <52738477+shakum001@users.noreply.github.com>
2019-12-17 15:19:35 +0530

    Update s3bucket.py

--- s3bucket.py
+++ s3bucket.py
@@ -1,3 +1,3 @@
 import boto3
-	s3 = boto3.client('s3',region_name = 'us-east-1')
-	s3.create_bucket(Bucket = 'my-bucket-wefkknf4')
+s3 = boto3.client('s3',region_name = 'us-east-1')
+s3.create_bucket(Bucket = 'my-bucket-wefkknf4')
